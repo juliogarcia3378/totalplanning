7057dad72dc19958e42874765fc7c4e293e9718b
System Administrator <root@users-MacBook-Pro-2407.local>
2016-08-03 14:38:40 -0700

    Toast changing

--- src/ADEPSOFT/ComunBundle/Resources/public/base/plugins/bootstrap-toastr/toastr.js
+++ src/ADEPSOFT/ComunBundle/Resources/public/base/plugins/bootstrap-toastr/toastr.js
@@ -113,15 +113,15 @@
 					debug: false,
 
 					showMethod: 'fadeIn', //fadeIn, slideDown, and show are built into jQuery
-					showDuration: 300,
+					showDuration: 11300,
 					showEasing: 'swing', //swing and linear are built into jQuery
 					onShown: undefined,
 					hideMethod: 'fadeOut',
-					hideDuration: 1000,
+					hideDuration: 111000,
 					hideEasing: 'swing',
 					onHidden: undefined,
 
-					extendedTimeOut: 1000,
+					extendedTimeOut: 111000,
 					iconClasses: {
 						error: 'toast-error',
 						info: 'toast-info',
@@ -130,7 +130,7 @@
 					},
 					iconClass: 'toast-info',
 					positionClass: 'toast-top-right',
-					timeOut: 5000, // Set timeOut and extendedTimeout to 0 to make it sticky
+					timeOut: 188000, // Set timeOut and extendedTimeout to 0 to make it sticky
 					titleClass: 'toast-title',
 					messageClass: 'toast-message',
 					target: 'body',
